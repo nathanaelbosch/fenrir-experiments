cc1152a51bc4cc04352c84775a681e294608ed02
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2022-06-01 14:27:59 +0300

    Fix 2/main.jl

--- experiments/2/main.jl
+++ experiments/2/main.jl
@@ -21,7 +21,6 @@ function f2(du, u, p, t)
     α, β, δ, γ = p
     du[1] = dx = α * x - β * x * y
     du[2] = dy = -δ * y
-    return nothing
 end
 prob2 = ODEProblem(f2, prob.u0, prob.tspan, prob.p)
 prob2 = ProbNumDiffEq.remake_prob_with_jac(prob2)
@@ -31,7 +30,6 @@ function f3(du, u, p, t)
     α, β, δ, γ = p
     du[1] = dx = α * x^2 - β * y
     du[2] = dy = -δ * y + γ * x * y
-    return nothing
 end
 prob3 = ODEProblem(f3, prob.u0, prob.tspan, prob.p)
 prob3 = ProbNumDiffEq.remake_prob_with_jac(prob3)
@@ -41,14 +39,11 @@ function f4(du, u, p, t)
     α, β, δ, γ = p
     du[1] = dx = α * x^2 - β * y
     du[2] = dy = -δ * y
-    return nothing
 end
 prob4 = ODEProblem(f4, prob.u0, prob.tspan, prob.p)
 prob4 = ProbNumDiffEq.remake_prob_with_jac(prob4)
 
-
 final_NLLs = Vector{Float64}[]
-
 
 for (i, _prob) in enumerate((prob1, prob2, prob3, prob4))
     push!(final_NLLs, Float64[])
@@ -66,21 +61,29 @@ for (i, _prob) in enumerate((prob1, prob2, prob3, prob4))
         )
 
         # Initial parameters, with both vector-field parameters and the log-diffusion
-        u0, κ²0 = get_initial_u0_diff(_prob, noisy_ode_data, tsteps)
-        u0 = min.(max.(u0, u0_bounds[1]), u0_bounds[2])
+        κ²0 = get_initial_diff(prob, noisy_ode_data, tsteps) # * ones(E)
+        u0 = clamp.(noisy_ode_data[1], u0_bounds[1], u0_bounds[2])
         p0 = θ_init()
         σ²0 = 1.0
         θ_start = [u0..., p0..., log(σ²0), log(κ²0)]
 
-
         @info "[Problem $i][$j]" θ_start
-        l, times, states = exact_nll(θ_start, opt_params);
+        vec2args(x) = (u0=x[1:D], p=x[D+1:end-2], σ²=exp.(x[end-1]), κ²=exp.(x[end]))
+        function loss(x, other_params)
+            @unpack u0, p, σ², κ² = vec2args(x)
+            @unpack prob, ode_data, tsteps, dt = other_params
+            data = (t=tsteps, u=noisy_ode_data)
+            return exact_nll(remake(prob, u0=u0, p=p), data, σ², κ²; dt=dt)
+        end
+        l, times, states = loss(θ_start, opt_params)
 
-        f = OptimizationFunction(exact_nll, GalacticOptim.AutoForwardDiff())
+        f = OptimizationFunction(loss, GalacticOptim.AutoForwardDiff())
         optprob = OptimizationProblem(
-            f, θ_start, opt_params;
+            f,
+            θ_start,
+            opt_params;
             lb=[u0_bounds[1]..., θ_bounds[1]..., log(1e-8), log(1e-20)],
-            ub=[u0_bounds[2]..., θ_bounds[2]..., log(1e3), log(1e50)]
+            ub=[u0_bounds[2]..., θ_bounds[2]..., log(1e3), log(1e50)],
         )
 
         @info "[Problem $i][$j] Starting the optimization"
@@ -88,8 +91,9 @@ for (i, _prob) in enumerate((prob1, prob2, prob3, prob4))
         # optimizer = LBFGS()
         optsol = solve(optprob, optimizer; maxiters=200)
         # optsol = solve(optprob, optimizer; maxiters=200, show_trace=true)
-        opt_u0, opt_p, opt_σ², opt_κ² = optsol.u[1:D], optsol.u[D+1:end-2], exp(optsol.u[end-1]), exp(optsol.u[end])
-        @info "[Problem $i][$j] Done" prob.p opt_p opt_u0 opt_σ² opt_κ²;
+        opt_u0, opt_p, opt_σ², opt_κ² =
+            optsol.u[1:D], optsol.u[D+1:end-2], exp(optsol.u[end-1]), exp(optsol.u[end])
+        @info "[Problem $i][$j] Done" prob.p opt_p opt_u0 opt_σ² opt_κ²
         @info "[Problem $i][$j] Final negative log-likelihood" optsol.minimum
 
         push!(final_NLLs[end], optsol.minimum)
