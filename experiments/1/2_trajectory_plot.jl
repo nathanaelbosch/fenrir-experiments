a1152fc721ec487667807501b83159ef1f21a423
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2022-06-01 15:47:21 +0300

    Clean up experiment 1 plotting scripts

--- experiments/1/2_trajectory_plot.jl
+++ experiments/1/2_trajectory_plot.jl
@@ -1,9 +1,7 @@
 using OrdinaryDiffEq, LinearAlgebra, Statistics
 using CairoMakie
-# using Plots
 using Fenrir
 
-# probname, noisestr = "lv", "high"
 probname, noisestr = "fhn", "high"
 
 include("../theme.jl")
@@ -14,107 +12,78 @@ RESULTS_DIR = joinpath(@__DIR__, "results")
 METHODS = ("ODIN_withIV", "Classic_withIV", "FENRIR_withIV")
 NOISE_LEVELS = ("low", "high")
 
-PROBLEMS = Dict(
-    "lv" => lotkavolterra,
-    "fhn" => fitzhughnagumo,
-    "pt" => protein_transduction,
-)
+PROBLEMS = Dict("lv" => lotkavolterra, "fhn" => fitzhughnagumo)
 problem = PROBLEMS[probname]
 prob, _, _, _, _, _ = problem()
 D = length(prob.u0)
 plot_ts = range(prob.tspan..., length=1000)
 true_sol = solve(prob, Tsit5(), abstol=1e-9, reltol=1e-6, saveat=plot_ts)
 
-
-
-
 get_params(filepath) = map(l -> parse.(Float64, l), split.(readlines(filepath), ","))
 parameters = Dict([
     (probname, noisestr, m) => get_params(
-        joinpath(RESULTS_DIR, "$(probname)_$(noisestr)noise_$(lowercase(m)).txt"))
-    for m in METHODS, noisestr in NOISE_LEVELS])
+        joinpath(RESULTS_DIR, "$(probname)_$(noisestr)noise_$(lowercase(m)).txt"),
+    ) for m in METHODS, noisestr in NOISE_LEVELS
+])
 
-
-
-# fig = Figure(
-#     resolution=(WIDTH, HEIGHT),
-#     figure_padding=5,
-# )
 axis_kwargs = (
     xgridvisible=false,
     ygridvisible=false,
-    # yticks=[1,6],
-    yticks=[-2,2],
+    yticks=[-2, 2],
     xticks=[prob.tspan[1], prob.tspan[2]],
-    xtrimspine=true, ytrimspine=true,
+    xtrimspine=true,
+    ytrimspine=true,
 )
-# ax1 = fig[1, 1] = Axis(fig; axis_kwargs...)
-# ax2 = fig[1, 2] = Axis(fig; axis_kwargs..., yticklabelsvisible=false)
-# ax3 = fig[1, 3] = Axis(fig; axis_kwargs..., yticklabelsvisible=false)
-# rowgap!(fig.layout, 15)
-# colgap!(fig.layout, 15)
-ax1 = Axis(gl[1,1]; axis_kwargs...,
-           xticklabelsvisible=false,
-           )
-ax2 = Axis(gl[2,1]; axis_kwargs...,
-           xticklabelsvisible=false,
-           )
-ax3 = Axis(gl[3,1]; axis_kwargs...,
-           # yticklabelsvisible=false,
-           )
+ax1 = Axis(gl[1, 1]; axis_kwargs..., xticklabelsvisible=false)
+ax2 = Axis(gl[2, 1]; axis_kwargs..., xticklabelsvisible=false)
+ax3 = Axis(gl[3, 1]; axis_kwargs...)
 axes = [ax1, ax2, ax3]
-
-
 
 sums = []
 for (i, m) in enumerate(METHODS)
-    # if m == "FENRIR" continue end
     ps = parameters[(probname, noisestr, m)]
-    eprob = EnsembleProblem(prob, prob_func=(prob, i, repeat)->remake(
-        prob, u0=ps[i][1:D], p=ps[i][D+1:end]))
-    sim = solve(eprob,
-                # Tsit5(),
-                RadauIIA5(),
-                EnsembleThreads();
-                trajectories=length(ps),
-                abstol=1e-9, reltol=1e-6,
-                )
+    eprob = EnsembleProblem(
+        prob,
+        prob_func=(prob, i, repeat) -> remake(prob, u0=ps[i][1:D], p=ps[i][D+1:end]),
+    )
+    sim = solve(
+        eprob,
+        RadauIIA5(),
+        EnsembleThreads();
+        trajectories=length(ps),
+        abstol=1e-9,
+        reltol=1e-6,
+    )
 
     solutions = cat([ProbNumDiffEq.stack(s.(plot_ts)) for s in sim.u]..., dims=3)
     medians = median(solutions, dims=3)
 
-    sum = EnsembleSummary(sim, plot_ts;
-                          # quantiles=[0.05, 0.95]
-                          quantiles=[0.10, 0.90]
-                          )
+    sum = EnsembleSummary(sim, plot_ts; quantiles=[0.10, 0.90])
 
     for d in 1:D
-        # c = COLORS[D*(i-1)+d]
         c = COLORS[i]
-        # for u in sim.u
-        #     lines!(axes[i], u.t, [_u[d] for _u in u.u],
-        #            color=(c, 1.0), linewidth=0.1)
-        # end
-        lines!(axes[i], sum.u.t,
-               # [u[d] for u in sum.u.u],
-               medians[:, d, 1],
-               linewidth=1,
-               color=(c, 1.0))
-        band!(axes[i], sum.u.t,
-              [u[d] for u in sum.qhigh.u],
-              [u[d] for u in sum.qlow.u],
-              color=(c, 0.4))
+        lines!(axes[i], sum.u.t, medians[:, d, 1], linewidth=1, color=(c, 1.0))
+        band!(
+            axes[i],
+            sum.u.t,
+            [u[d] for u in sum.qhigh.u],
+            [u[d] for u in sum.qlow.u],
+            color=(c, 0.4),
+        )
     end
 end
 
 for ax in (ax1, ax2, ax3)
     for d in 1:D
-        lines!(ax, true_sol.t, Array(true_sol)[d, :],
-               color=:black, linestyle=:dash, linewidth=0.5,
-               )
+        lines!(
+            ax,
+            true_sol.t,
+            Array(true_sol)[d, :],
+            color=:black,
+            linestyle=:dash,
+            linewidth=0.5,
+        )
     end
 end
 
 rowgap!(gl, 10)
-
-# save(joinpath(DIR, "results", "trajectories.pdf"), fig, pt_per_unit=1)
--- experiments/1/9_parameter_boxplots.jl
+++ experiments/1/9_parameter_boxplots.jl
@@ -6,16 +6,12 @@ using ProbNumDiffEq
 include("../theme.jl")
 DIR = @__DIR__
 
-WIDTH, HEIGHT = FULL_WIDTH, HALF_HEIGHT*0.9
+WIDTH, HEIGHT = FULL_WIDTH, HALF_HEIGHT * 0.9
 
 RESULTS_DIR = joinpath(@__DIR__, "results")
 RESULTS_DIR = "experiments/1/results"
-# run(`scp uni:\~/PhD-Projects/fenrir/code/experiments/1/results/\*.txt $(RESULTS_DIR)`)
 
-PROBLEMS = Dict(
-    "lv" => lotkavolterra,
-    "fhn" => fitzhughnagumo,
-)
+PROBLEMS = Dict("lv" => lotkavolterra, "fhn" => fitzhughnagumo)
 NOISE_LEVELS = ("low", "high")
 METHODS = ("ODIN_withIV", "Classic_withIV", "FENRIR_withIV")
 LABELS = Dict(
@@ -29,68 +25,71 @@ for probname in keys(PROBLEMS), noisestr in NOISE_LEVELS
     @info "Plotting" probname noisestr
     problem = PROBLEMS[probname]
 
-fig = Figure(
-    resolution=(WIDTH, HEIGHT),
-    figure_padding=5,
-)
-ax = fig[1, 1] = Axis(
-    fig;
-    ylabel=RELATIVE_ERROR ? "Relative error" : "Absolute error",
-    yscale=log10,
-    xgridvisible=false,
-    ygridvisible=true,
-    bottomspinevisible=false,
-    leftspinevisible=false,
-    ygridcolor=:black,
-    ygridwidth=0.5,
-    yticksvisible=false,
-    xticksvisible=false,
-    # xticks=[1e-2, 1e-1, 1e0],
-    xticklabelsize=BASE_FONTSIZE-1,
-    yticks= ([1e-4, 1e-3, 1e-2, 1e-1, 1e0, 1e1, 1e2],
-             ["10⁻⁴", "10⁻³", "10⁻²", "10⁻¹", "10⁰", "10¹", "10²"]),
-)
+    fig = Figure(resolution=(WIDTH, HEIGHT), figure_padding=5)
+    ax =
+        fig[1, 1] = Axis(
+            fig;
+            ylabel=RELATIVE_ERROR ? "Relative error" : "Absolute error",
+            yscale=log10,
+            xgridvisible=false,
+            ygridvisible=true,
+            bottomspinevisible=false,
+            leftspinevisible=false,
+            ygridcolor=:black,
+            ygridwidth=0.5,
+            yticksvisible=false,
+            xticksvisible=false,
+            # xticks=[1e-2, 1e-1, 1e0],
+            xticklabelsize=BASE_FONTSIZE - 1,
+            yticks=(
+                [1e-4, 1e-3, 1e-2, 1e-1, 1e0, 1e1, 1e2],
+                ["10⁻⁴", "10⁻³", "10⁻²", "10⁻¹", "10⁰", "10¹", "10²"],
+            ),
+        )
 
+    prob, (tsteps, _), _, _, _ = problem()
+    true_theta = [prob.u0..., prob.p...]
+    D = length(prob.u0)
+    P = length(prob.p)
 
-prob, (tsteps, _), _, _, _ = problem()
-true_theta = [prob.u0..., prob.p...]
-D = length(prob.u0)
-P = length(prob.p)
+    get_params(filepath) = map(l -> parse.(Float64, l), split.(readlines(filepath), ","))
+    parameters = Dict([
+        (m) => get_params(
+            joinpath(RESULTS_DIR, "$(probname)_$(noisestr)noise_$(lowercase(m)).txt"),
+        ) for m in METHODS
+    ])
+    params = [parameters[m] for m in METHODS]
+    N1, N2, N3 = length.([parameters[m] for m in METHODS])
+    all_params = vcat(ProbNumDiffEq.stack.([parameters[m] for m in METHODS])...)
 
+    abs_errors = abs.(all_params .- true_theta')
+    rel_errors = abs_errors ./ abs.(true_theta')
+    errors = RELATIVE_ERROR ? rel_errors : abs_errors
 
+    for j in 1:length(true_theta)
+        ys = errors[:, j]
+        xs = j * ones(Int, length(ys))
+        dodge = vcat(ones(Int, N1), 2 * ones(Int, N2), 3 * ones(Int, N3))
 
-get_params(filepath) = map(l -> parse.(Float64, l), split.(readlines(filepath), ","))
-parameters = Dict([(m) => get_params(joinpath(RESULTS_DIR, "$(probname)_$(noisestr)noise_$(lowercase(m)).txt")) for m in METHODS])
-params = [parameters[m] for m in METHODS]
-N1, N2, N3 = length.([parameters[m] for m in METHODS])
-all_params = vcat(ProbNumDiffEq.stack.([parameters[m] for m in METHODS])...)
+        boxplot!(
+            ax,
+            xs,
+            ys;
+            dodge=dodge,
+            color=[COLORS[d] for d in dodge],
+            BOXPLOT_KWARGS...,
+        )
+    end
 
-abs_errors = abs.(all_params .- true_theta')
-rel_errors = abs_errors ./ abs.(true_theta')
-errors = RELATIVE_ERROR ? rel_errors : abs_errors
+    labels = ["ODIN", "RK", "FENRIR"]
+    elements = [PolyElement(polycolor=COLORS[i]) for i in 1:length(labels)]
+    title = "Method"
+    Legend(fig[1, end+1], elements, labels, title)
 
-for j in 1:length(true_theta)
-    ys = errors[:, j]
-    xs = j * ones(Int, length(ys))
-    dodge = vcat(ones(Int, N1), 2*ones(Int, N2), 3*ones(Int, N3))
+    labels = LABELS[probname]
+    ax.xticks = (1:length(labels), labels)
 
-    boxplot!(
-        ax, xs, ys;
-        dodge=dodge,
-        color=[COLORS[d] for d in dodge],
-        BOXPLOT_KWARGS...
-            )
-end
+    trim!(fig.layout)
 
-labels = ["ODIN", "RK", "FENRIR"]
-elements = [PolyElement(polycolor = COLORS[i]) for i in 1:length(labels)]
-title = "Method"
-Legend(fig[1,end+1], elements, labels, title)
-
-labels = LABELS[probname]
-ax.xticks = (1:length(labels), labels)
-
-trim!(fig.layout)
-
-save(joinpath(DIR, "results", "p_errs_$(probname)_$(noisestr).pdf"), fig, pt_per_unit=1)
+    save(joinpath(DIR, "results", "p_errs_$(probname)_$(noisestr).pdf"), fig, pt_per_unit=1)
 end
